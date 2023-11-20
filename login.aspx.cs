6cc80d351213b750d2f4782e9b068e63208b1ec6
cadekellar <cadekellar@gmail.com>
2023-11-19 23:21:22 -0600

    Added Code for the Login and Member Page. (Finished)

--- login.aspx.cs
+++ login.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -13,10 +15,88 @@ namespace Assignment4
         {
 
         }
-
+        //Establishing Connection and Authentication 
+        SqlConnection dbcon;
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\cadek\\Desktop\\Kellar\\Assignment4-main\\Assignment4-main\\App_Data\\KarateSchool(1).mdf;Integrated Security=True;Connect Timeout=30";
         protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
+            string userName = Login1.UserName;
+            string pass = Login1.Password;
 
+            string userType = "";
+            string dbUser = "";
+            string dbPass = "";
+           
+            
+            //Getting User info from the table
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Open();
+                string query = "SELECT UserType, UserName, UserPassword FROM NetUser WHERE UserName = @UserName;";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@UserName", userName);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            userType = reader["UserType"].ToString();
+                            dbUser = reader["UserName"].ToString();
+                            dbPass = reader["Password"].ToString();
+                        }
+                    }
+                }
+            }
+            //Checks the role and sends to what they are 
+            if (userType == "Member" && dbUser == userName && dbPass == pass)
+            {
+                FormsAuthentication.RedirectFromLoginPage(userName, true);
+                Response.Redirect("member.aspx", true);
+            }
+            else if (userType == "Instructor" && dbUser == userName && dbPass == pass)
+            {
+                FormsAuthentication.RedirectFromLoginPage(userName, true);
+                Response.Redirect("instructor.aspx", true);
+            }
+            else if (userType == "Administrator" && dbUser == userName && dbPass == pass)
+            {
+                FormsAuthentication.RedirectFromLoginPage(userName, true);
+                Response.Redirect("administrator.aspx", true);
+            }
+            else
+                Response.Redirect("login.aspx", true);
+
+        }
+
+        //UserID get
+        public int GetUserID(string username)
+        {
+            string userIDStr = "";
+
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Open();
+                string query = "SELECT UserID FROM NetUSer WHERE UserName = @UserName;";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@UserName", username);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            userIDStr = reader["UserID"].ToString();
+
+                        }
+                    }
+                }
+            }
+
+            //String to integer
+            int userID = int.Parse(userIDStr);
+            return userID;
         }
     }
 }
